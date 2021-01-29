63f77afec27c687dc4b6f5aadb2514e30e881bee
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-01-29 14:53:57 -0800

    part1 #18 done

--- part1/part1-question18.js
+++ part1/part1-question18.js
@@ -11,3 +11,10 @@ let statistics = {
     blackCars: 40,
     rareCars: 2
 };
+
+for(const property in statistics) {
+    let start = property.charAt(0);
+    if(start == "r" || statistics[property]%2 == 1 ){
+        console.log(statistics[property]);
+    }
+}
