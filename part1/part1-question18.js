fcb8c1b937525fb4bb82ed5a75e8ece28130559a
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-01-28 16:37:14 -0800

    updated problem for 18..finish the code

--- part1/part1-question18.js
+++ part1/part1-question18.js
@@ -1,4 +1,6 @@
 //Finish the for in loop??
+//Given the below Object, write a for...in loop that will iterate through it 
+//and print out the value of the property if the property starts with the letter r, or if the value of that property is an odd number. 
 
 let statistics = {
     redCars: 21,
